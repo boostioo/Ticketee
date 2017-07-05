30f2a408a632c52ae2f6fd1ad3035a1ff8e49172
s3502942 <s3502942@student.rmit.edu.au>
2017-07-05 11:06:30 +0000

    ability to view a list of all projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,30 +1,31 @@
 class ProjectsController < ApplicationController
-    def index
-    end
-    
-    def new
-        @project = Project.new
-    end
-    
-    def create
-        @project = Project.new(project_params)
+  def index
+    @projects = Project.all
+  end
+  
+  def new
+      @project = Project.new
+  end
+  
+  def create
+      @project = Project.new(project_params)
 
-      if @project.save
-        flash[:notice] = "Project has been created."
-        redirect_to @project
-      else
-        flash.now[:alert] = "Project has not been created."
-        render "new"
-      end
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been created."
+      render "new"
     end
-    
-    def show
-        @project = Project.find(params[:id])
-    end
+  end
+  
+  def show
+      @project = Project.find(params[:id])
+  end
 
-    private
+  private
 
-    def project_params
-      params.require(:project).permit(:name, :description)
-    end
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
